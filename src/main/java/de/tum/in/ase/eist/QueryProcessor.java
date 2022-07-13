714b9f19ff9289e6a056dccb97fe67a36fed8bdd
jakobehlert <jakob.ehlert@tum.de>
2022-07-13 13:14:32 +0200

    Summation

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -10,6 +10,17 @@ public class QueryProcessor {
         if(query.contains("name")) {
             return "jake the snake";
         }
+        if(query.contains("what is")) {
+            if(query.contains("plus")) {
+                // what is 12 plus 3
+                // what is 2 plus 23
+                int first = Integer.parseInt(query.substring(8,10).strip());
+                int second = Integer.parseInt(query.substring(15).strip());
+                int result = first + second;
+                return String.valueOf(result);
+            }
+            return "11";
+        }
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
